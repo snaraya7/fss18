a8118d8b8189b6461bf973d59b7b6b18165c1999
snaraya7 <snaraya7@ncsu.edu>
2018-08-29 23:40:22 -0400

    few more test cases

--- w12/w1.py
+++ w12/w1.py
@@ -1,5 +1,7 @@
 import re,traceback
 import random as wheeloffortune
+import math
+from collections import Counter
 
 
 class O:
@@ -26,8 +28,113 @@ def double(x):
     return x * 2
 
 @O.k
+def testTuples():
+    my_tuple = (1, 2)
+    try:
+        my_tuple[1] = 3
+        modifyable = True
+    except TypeError:
+        modifyable = False
+
+    assert modifyable == False
+
+@O.k
+def testDict():
+
+    grades = {"Shri": 90, "kanth": 97}
+    assert grades["Shri"] == 90
+
+@O.k
+def testSet():
+   s = set()
+   s.add('apple')
+   s.add('Apple')
+   s.add('apple')
+   s.add('orange')
+
+   assert len(s) == 3
+
+@O.k
+def testCount():
+    c = Counter([1,1,1,3,4])
+    assert c[1] == 3
+
+@O.k
 def testFunctions():
     assert double(2) == 4
+
+@O.k
+def testControlFlow():
+    assert True if 10 % 2 == 0 else False
+
+
+@O.k
+def testTruthiness():
+
+    space = None
+
+    assert None == space
+
+@O.k
+def testRandom():
+    assert wheeloffortune.randrange(3, 6) >= 3 and wheeloffortune.randrange(3, 6) < 6
+
+def lazy_range(n):
+    """a lazy version of range"""
+    i = 0
+    while i < n:
+     yield i
+     i += 1
+
+def customGen():
+    return (i for i in lazy_range(20) if i % 2 == 0)
+
+@O.k
+def testGenerator():
+    evenList = list(customGen())
+    for e in evenList:
+        if e % 2 != 0:
+            assert False
+
+    assert True
+
+@O.k
+def testIte():
+
+    sample = [1,2]
+    i = 0
+    for s in sample:
+        sample[i] = sample[i] + 1
+        i = i + 1
+
+    assert sample[0] == 2 and sample[1] == 3
+
+@O.k
+def testRegex():
+    name = "shri2018"
+    passed = re.match(name, "shri2018kanth")
+    name = re.sub("[0-9]", "-", name)
+
+    assert passed and name == "shri----"
+
+@O.k
+def testListCompr():
+
+    squares = [x * x for x in range(5)]
+    test = False
+
+    for s in squares:
+        if  s !=0 and s / math.sqrt(s) != math.sqrt(s):
+            assert False
+
+    assert True
+
+
+
+@O.k
+def testSorting():
+    x = sorted([-4, 1, -2, 3], key=abs, reverse=True)
+    assert x[0] <= x[len(x) - 1]
 
 @O.k
 def testModules():
