b6dc6850339625a662b23f812d5e95434d24e417
snaraya7 <snaraya7@ncsu.edu>
2018-09-04 11:24:30 -0400

    hw2 init

---
+++ w12/w2.py
@@ -0,0 +1,164 @@
+def lines(s):
+  rows = []
+
+  for r in s.split('\n'):
+
+     try:
+        hashIndex = r.index('#')
+     except ValueError:
+         hashIndex = -1
+     if hashIndex > 0 :
+        rows.append(r[0:hashIndex].strip())
+     else :
+         rows.append(r.strip())
+
+  return rows
+
+
+
+def rows(src):
+  """Kill bad characters. If line ends in ','
+   then join to next. Skip blank lines."""
+
+  rows = []
+  newRow = ''
+  next = False
+  for r in src:
+
+    if r.endswith(','):
+        newRow += r
+        next = True
+    elif next:
+        newRow += r
+        next = False
+    else:
+        rows.append(r)
+
+  rows = [newRow] + rows
+  return rows
+
+
+def cols(src):
+  """ If a column name on row1 contains '?',
+  then skip over that column."""
+
+  cleanRows = []
+  for r in src:
+      if len(r.strip()) > 0:
+          cleanRows.append(r)
+
+
+  filteredCols = []
+  colSkipIndex = -1
+  for s in cleanRows:
+      rowNames = s.split(",")
+      for ss in rowNames:
+          colSkipIndex = colSkipIndex + 1
+          if '?' in ss:
+              break
+      break
+
+  for r in cleanRows:
+
+      fRow = ""
+      currentIndex = 0
+      for s in r.split (","):
+          if colSkipIndex != currentIndex:
+           fRow = fRow + s + ","
+
+          currentIndex = currentIndex + 1
+
+      filteredCols.append(fRow[:-1])
+
+  return filteredCols
+
+
+def prep(src):
+  """ If a column name on row1 contains '$',
+  coerce strings in that column to a float."""
+  cleanRows = []
+  for r in src:
+      if len(r.strip()) > 0:
+          cleanRows.append(r)
+
+  filteredCols = []
+  colSkipIndex = -1
+  for s in cleanRows:
+      rowNames = s.split(",")
+      for ss in rowNames:
+          colSkipIndex = colSkipIndex + 1
+          if '$' in ss:
+              break
+      break
+
+  header = True
+
+  for r in cleanRows:
+
+      fRow = ""
+      currentIndex = 0
+      for s in r.split(","):
+          if colSkipIndex == currentIndex:
+              if header:
+                  fRow = fRow + s + ","
+                  header = False
+              else:
+                  fRow = fRow + str(float(s)) + ","
+          else:
+              fRow = fRow + s + ","
+
+          currentIndex = currentIndex + 1
+
+      filteredCols.append(fRow[:-1])
+
+  return filteredCols
+
+DATA1 = """
+  outlook,$temp,?humidity,windy,play
+  sunny,85,85,FALSE,no
+  sunny,80,90,TRUE,no
+  overcast,83,86,FALSE,yes
+  rainy,70,96,FALSE,yes
+  rainy,68,80,FALSE,yes
+  rainy,65,70,TRUE,no
+  overcast,64,65,TRUE,yes
+  sunny,72,95,FALSE,no
+  sunny,69,70,FALSE,yes
+  rainy,75,80,FALSE,yes
+  sunny,75,70,TRUE,yes
+  overcast,100,25,90,TRUE,yes
+  overcast,81,75,FALSE,yes
+  rainy,71,91,TRUE,no"""
+
+DATA2 = """
+      outlook,   # weather forecast. 
+      $temp,     # degrees farenheit
+      ?humidity, # relative humidity
+      windy,     # wind is high
+      play       # yes,no
+      
+      sunny,85,85,FALSE,no
+      sunny,80,90,TRUE,no
+      overcast,83,86,FALSE,yes
+      rainy,70,96,FALSE,yes
+      rainy,68,80,FALSE,yes
+      rainy,65,70,TRUE,no
+      overcast,64,65,TRUE,yes
+      sunny,72,95,FALSE,no
+      sunny,69,70,FALSE,yes
+      rainy,75,80,FALSE,yes
+      sunny,75,70,TRUE,yes
+      overcast,100,25,90,TRUE,yes
+      overcast,81,75,FALSE,yes # unique day
+      rainy,71,91,TRUE,no """
+
+def main():
+    result = cols(rows(lines(DATA2)))
+    for r in result:
+        print(r)
+    # var = " asdasd  ?asdasd"
+    # if '?' in var:
+    #     print(True)
+    # else:
+    #     print(False)
+main()
