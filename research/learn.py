6322c2eeadcfa4d4c1e6fdb614347f168dd38439
snaraya7 <snaraya7@ncsu.edu>
2018-09-12 19:19:32 -0400

    w3 init

---
+++ research/learn.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn import tree
+import graphviz
+
+
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(iris.data, iris.target)
+
+dot_data = tree.export_graphviz(clf, out_file=None)
+graph = graphviz.Source(dot_data)
+graph.render("iris")
+
+dot_data = tree.export_graphviz(clf, out_file="shrikanth.dot",
+                         feature_names=iris.feature_names,
+                         class_names=iris.target_names,
+                         filled=True, rounded=True,
+                         special_characters=True)
+graph = graphviz.Source(dot_data)
+print(graph)
+print( clf.predict(iris.data[:1, :]))
