670b6fe09a47df76726fc1c89953077a4095b0d7
snaraya7 <snaraya7@ncsu.edu>
2018-09-14 13:14:55 -0400

    updated sample

--- w13/sample.py
+++ w13/sample.py
@@ -8,73 +8,80 @@
 # -- called `some`.
 # --
 
+import random,math
+from w13.TestEngine import O
 
 class sample:
 
+    def __init__(self, max = 1000, txt = ""):
+            self.max = max
+            self.rank = 1
+            self.txt = txt
+            self.n = 0
+            self.sorted = False
+            self.some = []
 
-
-def sample(max,txt) :
-  return
-#   {max=max or Lean.sample.max,
-#           rank=1, txt=txt or txt,
-#           n=0, sorted=false, some={}}
-# end
-
-# -- Note that the larger the `max`, the more
-# -- accurate the sample. For example, here's
-# -- what happens when `max` is 16 to 1024
-# -- and 10,000 times, we add in a random number
-# -- 0..1 (which should have a mean of 0.5)
-# --
-# --        16        0.29368748203557
-# --        32        0.58862982717745
-# --        64        0.49891762831198
-# --        128       0.43968772070468
-# --        256       0.51844127640056
-# --        512       0.50585840060648
-# --        1024      0.51028261217767
-# --
-#
-# -- Obviously, we can add up to `max` items, very simply.
-# -- But after that, we have to overwrite something
-# -- to fit in the new thing (which we do at probablity
-# -- `size(some)/n` items) where `n` is number of
-# -- times we've tried to add something here.
-
-def sampleInc(t,x,      now):
-#   t.n = t.n + 1
-#   now = #t.some
-#   if now < t.max then
-#     t.sorted = false
-#     t.some[ #t.some+1 ] = x
-#   elseif rand() < now/t.n then
-#     t.sorted = false
-#     t.some[ math.floor(0.5+ rand()*now) ] = x end
-  return #x
-# end
+    def sampleInc(self,x):
+        now = len(self.some)
+        self.n = self.n + 1
+        if now < self.max :
+            self.sorted = False
+            # self.some[ len(self.some) + 1 ] = x
+            self.some.append(x)
+        elif random.random() < now/self.n:
+            self.sorted = False
+            self.some[ math.floor(0.5+ random.random()* now) ] = x
+        return x
 
 # -- Never resort if we are already sorted.
 
-def sampleSorted(t):
-#   if not t.sorted then t.sorted=true; table.sort(t.some) end
-    return #t.some
-# end
+    def sampleSorted(self):
+        if  self.sorted == False:
+            self.sorted=True
+            self.some.sort()
+
+        return self.some
 
 # -- But once they are sorted, we can access the `nth`
 # -- percentile item.
 
-def nth(t,n,    s):
-  s = sampleSorted(t)
-  return #s[ min(#s, max(1, math.floor(0.5 + #s*n))) ]
-# end
+    def nths(self, ns):
 
-def nths(t, ns,out):
-#   ns = ns or {0.1,0.3,0.5,0.7,0.9}
-#   out={}
-#   for _,n in pairs(ns) do out[#out+1] = nth(t,n) end
-   return #out
-# end
+        ns = ns or {0.1,0.3,0.5,0.7,0.9}
+        out = []
+        for value in ns:
+            out[ len(out) + 1 ] = self.nth(value)
 
-def sampleLt(s1,s2):
-  return #nth(s1,0.5) < nth(s2,0.5)
-# end
+        return out
+
+
+    def nth(self,n):
+
+        data = self.sampleSorted()
+
+        x = min(len(data), max(1, math.floor(0.5 + len(data)*n)))
+        y = len(data)
+        return data[ min(len(data), max(1, math.floor(0.5 + len(data)*n))) ]
+
+    def sampleLt(s1,s2):
+            return (s1.nth(0.5) < s2.nth(0.5))
+
+
+@O.k
+def test():
+    random.seed(1)
+    samp = sample()
+    s = {}
+
+    for i in range (5,10):
+        s[i] = math.pow(2,i)
+
+    for i in range (1,10):
+        y = random.random()
+        for key, value in s.items():
+            samp.sampleInc(y)
+
+    for key, value in s.items():
+        print ( samp.nth(0.5) - 0.5 )
+        assert abs(samp.nth(0.5) - 0.5) < 0.2
+
